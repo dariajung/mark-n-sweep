800bd72ec1a17c44394e5f672141f42a3764a9c8
Daria Jung <djj2115cu@gmail.com>
2015-02-16 20:10:52 -0500

    start off with object that can only be Int

--- ms.c
+++ ms.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <assert.h>
 
+typedef enum {
+    OBJ_INT
+    //OBJ_PAIR
+} ObjectType;
+
+typedef struct sObject {
+    ObjectType type;
+    int value;
+} Object;
+
 int main(void) {
 
     return 0;
