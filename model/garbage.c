ff3a843d008b9db011346388e2919444d9a3bee4
Daria Jung <djj2115@columbia.edu>
2015-03-09 17:58:52 -0400

    rethink "heap" space

--- model/garbage.c
+++ model/garbage.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define HEAP_SIZE   100
 
@@ -15,13 +16,17 @@ typedef struct heap_object {
     // type - 1 for cons
     unsigned char type;
 
+    // null - 0 for non-null
+    // null - 1 for null
+    unsigned char null;
+
     // marked - 0 if not marked
     // marked - 1 if marked
     unsigned char marked;
 
     // 0 if not a root object
     // 1 if a root object
-    unsigned char root;
+    //unsigned char root;
 
     // "address" in FREE_LIST, 
     // or index in the free_list array
@@ -33,6 +38,7 @@ typedef struct heap_object {
         struct {
             struct heap_object *car;
             struct heap_object *cdr;
+            // if car or cdr is NULL, what to do?
         };
     };
 } HEAP_OBJECT;
@@ -40,21 +46,15 @@ typedef struct heap_object {
 
 // total size of heap is 100 cons_object objects
 // make this global
+// let's pre-allocate memory
 struct free_storage {
-    HEAP_OBJECT free_list[HEAP_SIZE];
-    int next_free_index;
-
+    unsigned char *heap_space;
+    int current_write_spot;
 } FREE_LIST;
 
 void init_heap() {
-
-    FREE_LIST.next_free_index = 0; 
-}
-
-void update_heap(HEAP_OBJECT new_object) {
-    int index = FREE_LIST.next_free_index;
-    FREE_LIST.free_list[index] = new_object;
-    FREE_LIST.next_free_index++;
+    FREE_LIST.heap_space = malloc(sizeof(struct heap_object) * HEAP_SIZE);
+    FREE_LIST.current_write_spot = 0; 
 }
 
 void init_bitarray() {
@@ -69,32 +69,106 @@ void update_bitarray() {
     BITARRAY.curr_free_index++;
 }
 
+void print_bitarray() {
+    int i;
+    printf("\r\n");
+    for (i = 0; i < HEAP_SIZE; i++) {
+        printf("%d", BITARRAY.bitarr[i]);
+    }
+    printf("\r\n");
+}
 
-// we will use sequential/first fit allocation for now since it is the simplest
-// void allocate() {
+// need a way to cycle through heap if next slot isn't free
+void update_heap(HEAP_OBJECT *new_object) {
 
-// }
+    int index = (FREE_LIST.current_write_spot) / sizeof(HEAP_OBJECT);
+
+    if (index >= HEAP_SIZE) {
+        printf("Whoops, went too far along the heap\n");
+        exit(1);
+    }
+
+    // FREE_LIST.heap_space + FREE_LIST.current_write_spot = new_object;
+    // FREE_LIST.current_write_spot += sizeof(HEAP_OBJECT);
+
+    update_bitarray();
+}
+
+// creates, doesn't set value
+HEAP_OBJECT * create_heap_object() {
+    HEAP_OBJECT *ptr = (HEAP_OBJECT *)(FREE_LIST.heap_space + FREE_LIST.current_write_spot);
+    ptr->marked = 0;
+    ptr->address = FREE_LIST.current_write_spot;
+
+    FREE_LIST.current_write_spot += sizeof(HEAP_OBJECT);
+
+    update_heap(ptr);
+
+    return ptr;
+}
+
+// 
+HEAP_OBJECT * create_integer(int value) {
+    HEAP_OBJECT *ptr = create_heap_object();
+    ptr->type = 0; // 0 because int
+    ptr->value = value; // set value to value
+
+    return ptr;
+}
+
+HEAP_OBJECT * create_NULL() {
+    HEAP_OBJECT *ptr = create_heap_object();
+    // set nothing
+    ptr->null = 1;
+
+    return ptr;
+}
+
+HEAP_OBJECT * create_cons(HEAP_OBJECT *car, HEAP_OBJECT *cdr) {
+    HEAP_OBJECT *ptr = create_heap_object();
+    ptr->type = 1;
+    ptr->car = car;
+    ptr->cdr = cdr;
+
+    return ptr;
+}
+
+void print_object(HEAP_OBJECT *obj) {
+
+    switch(obj->type) {
+        case 0:
+            if (obj->null == 1) {
+                printf("NIL");
+            } else {
+                printf("%d", obj->value); 
+            }
+            break;
+
+        case 1:
+            printf("(");
+            print_object(obj->car);
+            printf(", ");
+            print_object(obj->cdr);
+            printf(")");
+            break;
+    }
+}
 
 int main() {
-
-    printf("size of cons_object %d\n", (int)sizeof(HEAP_OBJECT));
-
-    printf("size of HEAP %d\n", (int)sizeof(FREE_LIST));
 
     //init bitarray
     init_bitarray();
 
     init_heap();
-    HEAP_OBJECT new_object;
-    new_object.type = 0;
-    new_object.marked = 0;
-    new_object.address = 0;
-    new_object.root = 1;
-    new_object.value = 10;
+    HEAP_OBJECT *new_object = create_integer(7);
+    HEAP_OBJECT *null_obj = create_NULL();
+    HEAP_OBJECT *nptr = create_cons(new_object, null_obj);
 
-    update_heap(new_object);
-    printf("%d\n", FREE_LIST.next_free_index);
+    print_object(nptr);
 
+    print_bitarray();
+
+    free(FREE_LIST.heap_space);
 
     return 0;
 }
